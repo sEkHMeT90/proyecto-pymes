c7673e7d5b279d8bad4d09621a18d5245dd80cbd
andresmarotta@gmail.com <andresmarotta@gmail.com@9abaed28-59f0-d009-731d-c6b51112cabf>
2012-02-09 23:59:11 +0000

    Ya se cargan las citas automáticamente en el DataGridView del formulario Principal (Falta filtrar por fecha). Modificado el formulario Principal y las clases: Especialidad, NuevaCita, Cita, Cliente, NuevoEmpleado, Trabajador y Usuario. Modificada la Base de Datos (María le agregó información para ir probando).

--- trunk/Proyecto_ADO_Pymes/Principal.vb
+++ trunk/Proyecto_ADO_Pymes/Principal.vb
@@ -7,13 +7,89 @@ Option Strict On
 ''' <author>María Navarro, Pedro Zalacain</author>
 Public Class Principal
 
-    Private HoraInicial As Integer
-    Private HoraFinal As Integer
+    Private _HoraInicial As Integer
+    Private _HoraFinal As Integer
+    Private _Trabajadores As List(Of Trabajador)
+    Private _Citas As List(Of Cita)
+
+    ''' <summary>
+    ''' Actualiza los datos del DataGrid
+    ''' </summary>
+    ''' <author>Andrés Marotta</author>
+    Private Sub Actualizar()
+        Dim minutos As Integer = 0
+        Dim hora As Integer = Me._HoraInicial - 1
+
+        Me._Citas = Cita.Cargar
+
+        ' Agrego tantas filas como horas, divididas en cuartos, haya
+        For i = 0 To (Me._HoraFinal - Me._HoraInicial) * 4
+            Me.DGVCitas.Rows.Add()
+        Next
+
+        ' Recorro cada fila del DataGridView
+        For fila = 0 To Me.DGVCitas.RowCount - 1
+
+            For columna = 0 To Me.DGVCitas.ColumnCount - 1
+                For Each cita In Me._Citas
+                    If cita.Hora = hora And cita.Minutos = minutos Then
+                        If CStr(cita.Trabajador.Codigo) = Me.DGVCitas.Columns(columna).HeaderText Then
+                            Me.DGVCitas.Rows(fila).Cells(columna).Value = cita.Cliente.Apellido1
+                        End If
+                    End If
+                Next
+            Next
+
+            If minutos < 45 Then
+                minutos += 15
+            Else
+                minutos = 0
+            End If
+
+            If fila Mod 4 = 0 Then
+                hora += 1
+            End If
+        Next
+
+        'If Me._Citas.Count > 0 Then
+        '    For i = 0 To Me.DGVCitas.RowCount - 1
+
+        '    Next
+        'End If
+
+        'If Me._Citas.Count > 0 Then
+        '    For i = 0 To Me._Citas.Count - 1
+        '        For j = 0 To Me._Trabajadores.Count - 1
+        '            If Me._Citas(i).Trabajador.Codigo = j Then
+        '                Me.DGVCitas.Rows(i).Cells(j).Value = Me._Citas(i).Cliente.Apellido1
+        '            End If
+        '        Next
+        '    Next
+        'Else
+        '    MsgBox("No hay citas")
+        'End If
+    End Sub
+
+    ''' <summary>
+    '''  Prepara el DataGrid para mostrar datos
+    ''' </summary>
+    ''' <author>Andrés Marotta</author>
+    Private Sub PrepararDataGrid()
+        Me._Trabajadores = Trabajador.Cargar()
+
+        Me.DGVCitas.ColumnCount = Me._Trabajadores.Count
+
+        For i = 0 To Me._Trabajadores.Count - 1
+            Me.DGVCitas.Columns(i).HeaderText = CStr(Me._Trabajadores(i).Codigo)
+        Next
+    End Sub
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        HoraInicial = 8
-        HoraFinal = 20
+        _HoraInicial = 8
+        _HoraFinal = 20
         Timer1.Start()
+        PrepararDataGrid()
+        Actualizar()
     End Sub
 
     ''' <summary>
@@ -55,35 +131,35 @@ Public Class Principal
     ''' </summary>
     ''' <author>María Navarro, Pedro Zalacain</author>
     Private Sub NuevoToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-        NuevoCliente.Show()
+        NuevoCliente.ShowDialog()
     End Sub
 
     Private Sub PBUsuario_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PBUsuario.Click
-        NuevoCliente.Show()
+        NuevoCliente.ShowDialog()
     End Sub
 
     Private Sub PBCalendario_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PBConsulta.Click
-        Consulta.Show()
+        Consulta.ShowDialog()
     End Sub
 
     Private Sub PBBuscar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PBBuscar.Click
-        Buscar.Show()
+        Buscar.ShowDialog()
     End Sub
 
     Private Sub PBCita_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PBCita.Click
-        NuevaCita.Show()
+        NuevaCita.ShowDialog()
     End Sub
 
     Private Sub PBFactura_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PBFactura.Click
-        Facturacion.Show()
+        Facturacion.ShowDialog()
     End Sub
 
     Private Sub FacturaciónToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FacturaciónToolStripMenuItem.Click
-        Facturacion.Show()
+        Facturacion.ShowDialog()
     End Sub
 
     Private Sub NuevaToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NuevaToolStripMenuItem.Click
-        NuevaEspecialidad.Show()
+        NuevaEspecialidad.ShowDialog()
     End Sub
 
     Private Sub NuevoToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NuevoToolStripMenuItem1.Click
@@ -91,15 +167,15 @@ Public Class Principal
     End Sub
 
     Private Sub NuevoToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NuevoToolStripMenuItem2.Click
-        NuevoCliente.Show()
+        NuevoCliente.ShowDialog()
     End Sub
 
     Private Sub NuevoToolStripMenuItem3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NuevoToolStripMenuItem3.Click
-        NuevoEmple.Show()
+        NuevoEmple.ShowDialog()
     End Sub
 
     Private Sub NuevaToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NuevaToolStripMenuItem1.Click
-        NuevaCita.Show()
+        NuevaCita.ShowDialog()
     End Sub
 
     Private Sub ModificarToolStripMenuItem4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ModificarToolStripMenuItem4.Click
@@ -116,5 +192,13 @@ Public Class Principal
 
     Private Sub SalirToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SalirToolStripMenuItem.Click
         Application.Exit()
+    End Sub
+
+    Private Sub ModificarToolStripMenuItem2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ModificarToolStripMenuItem2.Click
+        ModificarCliente.ShowDialog()
+    End Sub
+
+    Private Sub ModificarToolStripMenuItem3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ModificarToolStripMenuItem3.Click
+        ModificarEmpleado.ShowDialog()
     End Sub
 End Class
