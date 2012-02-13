33a0732e9f2e54a9dc68f13199646be6a5d7f9fb
pedro.zalacain@gmail.com <pedro.zalacain@gmail.com@9abaed28-59f0-d009-731d-c6b51112cabf>
2012-02-13 12:24:04 +0000

    Terminado Formulario CONSULTA

--- trunk/Proyecto_ADO_Pymes/Consulta.vb
+++ trunk/Proyecto_ADO_Pymes/Consulta.vb
@@ -29,13 +29,10 @@ Public Class Consulta
 
         ' Compruebo el siguiente estado
         If Estado = Semanal Then
-
+            DatosConsulta.ScrollBars = ScrollBars.Vertical
             Dim DiasSemana(7) As Integer
             DatosConsulta.ColumnCount = 7
-
-            For N = 0 To 14
-                DatosConsulta.Rows.Add()
-            Next
+            DatosConsulta.RowTemplate.Height = 22
 
             ' Memorizo los dias de la semana en un Array
             Select Case CInt(MCalendar.SelectionRange.Start.DayOfWeek)
@@ -110,6 +107,7 @@ Public Class Consulta
                 If Validaciones.EsFecha(Fecha) Then
                     ListaCitas = Cita.Cargar(CDate(Fecha))
                     For Y = 0 To ListaCitas.Count - 1
+                        DatosConsulta.Rows.Add()
                         DatosConsulta.Rows(Y).Cells(N).Value = ListaCitas(Y).Servicios(0).Descripcion
                         DatosConsulta.Rows(Y).Cells(N).ToolTipText = CStr(ListaCitas(Y).Fecha) & " - " & ListaCitas(Y).Hora
                     Next
@@ -120,6 +118,8 @@ Public Class Consulta
 
 
         ElseIf Estado = Mensual Then
+            DatosConsulta.ScrollBars = ScrollBars.Vertical
+            DatosConsulta.RowTemplate.Height = 22
             Dim MesConsulta As Integer = MCalendar.SelectionRange.Start.Month
             ListaCitas = Cita.ObtenerPorMes(MesConsulta)
 
@@ -134,10 +134,35 @@ Public Class Consulta
             DatosConsulta.Columns(6).HeaderText = "Servicios"
 
             For Each Cit As Cita In ListaCitas
-                DatosConsulta.Rows.Add(Cit.Codigo, Cit.Cliente.Apellido1, Cit.Trabajador.Apellido1, Cit.Fecha, Cit.Hora, Cit.Duracion, Cit.Servicios(0).Descripcion)
+                DatosConsulta.Rows.Add(Cit.Codigo, Cit.Cliente.Apellido1, Cit.Trabajador.Apellido1, Cit.Fecha, Cit.Hora.Hour.ToString("00") & ":" & Cit.Hora.Minute.ToString("00"), Cit.Duracion, Cit.Servicios(0).Descripcion)
             Next
         Else
+            DatosConsulta.ScrollBars = ScrollBars.None
+            Dim Meses() As String = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"}
+            DatosConsulta.RowTemplate.Height = 175
+            DatosConsulta.ColumnCount = 6
+            DatosConsulta.Columns(0).HeaderText = "Año " & MCalendar.SelectionRange.Start.Year
+            DatosConsulta.Rows.Add(Meses(0), Meses(1), Meses(2), Meses(3), Meses(4), Meses(5))
+            DatosConsulta.Rows.Add(Meses(6), Meses(7), Meses(8), Meses(9), Meses(10), Meses(11))
 
+            For N = 0 To 1
+                For Y = 0 To 5
+                    Dim Mes, Anio As Integer
+                    If N = 0 Then
+                        Mes = Y + 1
+                        Anio = MCalendar.SelectionRange.Start.Year
+                    Else
+                        Mes = Y + 7
+                        Anio = MCalendar.SelectionRange.Start.Year
+                    End If
+                    If Cita.HayCitas(Mes, Anio) Then
+                        DatosConsulta.Rows(N).Cells(Y).Style.BackColor = Color.Green
+                    Else
+                        DatosConsulta.Rows(N).Cells(Y).Style.BackColor = Color.Red
+                    End If
+                Next
+            Next
+            DatosConsulta.ClearSelection()
         End If
 
         LblVista.Text = Estado
